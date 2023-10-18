7f0a2cb403d43d307c8cea9f6074cabcf619591d
AnisaAminjonovna <amrianisa805@gmail.com>
2023-10-18 16:30:15 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,148 @@
+let mevalar = prompt("meva nomini kriting")
+
+switch (mevalar) {
+    case "o'rik":
+        alert("apricot")
+        break;
+
+case "avokado":
+        alert("avocado")
+        break;
+
+case "behi":
+        alert("quince")
+        break;
+
+case "olxo`ri":
+        alert("plum")
+        break;
+
+case "olcha":
+        alert("cherry")
+        break;
+
+case "ananas":
+        alert("pineapple")
+        break;
+
+case "apelsin":
+        alert("orange")
+        break;
+
+case "yeryong'oq":
+        alert("peanut")
+        break;
+
+case "tarvuz":
+        alert("watermelon")
+        break;
+
+case "banan":
+        alert("banan")
+        break;
+
+case "olma":
+        alert("apple")
+        break;
+
+case "xurmo":
+        alert("date")
+        break;
+
+case "anjir":
+        alert("figs")
+        break;
+
+case "greypfrut":
+        alert("grapefruit")
+        break;
+
+case "uzum":
+        alert("grape")
+        break;
+
+case "kivi":
+        alert("kiwi")
+        break;
+
+case "Laym":
+        alert("lime")
+        break;
+
+case "limon":
+        alert("lemon")
+        break;
+
+case "mango":
+        alert("mango")
+        break;
+
+case "qovun":
+        alert("melon")
+        break;
+
+case "nektarin":
+        alert("nectarine")
+        break;
+
+case "nok":
+        alert("pear")
+        break;
+
+case "papaya":
+        alert("papaya")
+        break;
+
+case "shaftoli":
+        alert("peach")
+        break;
+
+case "anor":
+        alert("pomegranate")
+        break;
+
+case "marakuya":
+        alert("passion fruit")
+        break;
+
+case "mandarin":
+        alert("tangerine")
+        break;
+
+case "golubika":
+        alert("blueberry")
+        break;
+
+case "ежевика":
+        alert("blackberry")
+        break;
+
+case "smorodina":
+        alert("currant")
+        break;
+
+case "klyukva":
+        alert("cranberry")
+        break;
+
+case "malina":
+        alert("raspberry")
+        break;
+
+case "qulupnay":
+        alert("strawberry")
+        break;
+
+case "kokos":
+        alert("coconut")
+        break;
+
+case "драгонфрут":
+        alert("dragonfruit")
+        break;
+        
+
+    default:
+        alert("bunaqa meva bazamizga kritilmagan")
+        break;
+}
